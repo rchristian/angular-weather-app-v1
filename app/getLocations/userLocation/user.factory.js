cb1435bc065f9ca631a29bddd066511f3e529cb3
Rachel <racheldchristian@gmail.com>
2016-07-19 13:10:22 -0500

    coded user location factory

---
+++ app/getLocations/userLocation/user.factory.js
@@ -0,0 +1,24 @@
+(function() {
+	'use strict';
+
+	angular
+		.factory('userLocationFactory', userLocationFactory);
+
+	userLocationFactory.$inject = ['$http'];
+ 
+	function userLocationFactory($http) {
+		return {
+			getUserLocation: getUserLocation
+		};
+
+		function getUserLoc() {
+			return $http.jsonp('http://ipinfo.io/json?callback=JSON_CALLBACK');
+				.then(getUserLocationComplete);
+
+			function getUserLocationComplete(response) {
+				var userLocation = response.data;
+				return userLocation;
+			}
+		}
+	}	
+})();
