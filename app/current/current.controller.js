22099ecf1ee882278cc5dfb008f2e695b4be5d40
Rachel <racheldchristian@gmail.com>
2016-07-19 13:06:19 -0500

    deleted controller code

---
+++ app/current/current.controller.js
@@ -0,0 +1,14 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app.current')
+		.controller('CurrentController', CurrentController);
+
+
+	function CurrentController($scope, userData, currentFactory) {
+		var vm = this;
+
+
+	}
+})();
