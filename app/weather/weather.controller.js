58a3aeef77a5bd78eea9eb739d0fb036cc5ae987
Rachel <racheldchristian@gmail.com>
2016-08-27 18:53:48 -0500

    removed unneeded code

--- app/weather/weather.controller.js
+++ app/weather/weather.controller.js
@@ -5,12 +5,6 @@
 		.module('app.weather')
 		.controller('WeatherController', WeatherController);
 
-	WeatherController.$inject = ['weatherFactory'];
+	function WeatherController() {}
 
-
-	function WeatherController(weatherFactory) {
-		/* jshint validthis: true */
-		var vm = this;
-	}
-	
 })();
