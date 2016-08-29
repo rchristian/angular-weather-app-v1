da669dcaa45cd1d339afafbd9b62d539861dd689
Rachel <racheldchristian@gmail.com>
2016-08-28 20:15:30 -0500

    fixed location call; changed getWeather to weather

--- app/core/weather.factory.js
+++ app/core/weather.factory.js
@@ -9,48 +9,27 @@
 
 	function weatherFactory ($http, userLocationFactory) {
 		return {
-			getWeather: getWeather,
+			weather: weather,
 		};
 
-		function getWeather() {
-			
-			var URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
-			var API = '&APPID=f72650c40fef189a346723016e3b5ca6';
-			var tempUnits = '&units=imperial';
-			var callback = '&callback=JSON_CALLBACK';
-			return $http.jsonp(URL + location + API + tempUnits + callback)
-				.then(getWeatherComplete)
-				//.catch("WeatherService Error");
-				//"cod":401
+		function weather() {
+			return userLocationFactory.getUserLocation().then(fetchWeather);
 
-			function getWeatherComplete(response) {
-				return response.data;
+			function fetchWeather(data) {
+				var URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+				var zipCountry = data.postal + ',' + data.country;
+				var API = '&APPID=f72650c40fef189a346723016e3b5ca6';
+				var tempUnits = '&units=imperial';
+				var callback = '&callback=JSON_CALLBACK';
+				return $http.jsonp(URL + zipCountry + API + tempUnits + callback)
+					.then(getWeatherComplete)
+					//.catch("WeatherService Error");
+					//"cod":401
+
+				function getWeatherComplete(response) {
+					return response.data;
+				}
 			}
 		}
 	}
 })();
-
-/*
-var globalVariable;
-function function1()
-{
-  globalVariable=12;
-  function2();
-}
-
-function function2()
-{
-  var local = globalVariable;
-}
-
-function activateWeather() {
-	return userLocationFactory.getUserLocation().then(fetchZipCountry);
-
-	function fetchZipCountry(data) {
-		var zipCountry = data.zip + ',' + data.countryCode;
-		weatherFactory.getWeather(zipCountry).then(function(data) {
-			weatherInfo(data);
-		});
-	}
-}
-*/
