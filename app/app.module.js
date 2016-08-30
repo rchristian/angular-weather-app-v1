fa800986f94d60452a962c55916530588b74641f
Rachel <racheldchristian@gmail.com>
2016-08-30 15:07:37 -0500

    removed ng-route

--- app/app.module.js
+++ app/app.module.js
@@ -3,22 +3,10 @@
 
 	angular
 		.module("app", [
-			//Angular modules
-			'ngRoute',
 			//Custom modules
 			'app.core',
 			'app.header',
 			'app.topBar',
 			'app.weather'
 		])
-		.config(configFunction);
-
-	configFunction.$inject = ['$routeProvider'];
-
-	function configFunction($routeProvider) {
-		$routeProvider.otherwise({
-			redirectTo: '/'
-		});
-	}
-
 })();
