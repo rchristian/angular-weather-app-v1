b563e0f6e8b8b9225428759ed30ecc2e6119cf17
Rachel <racheldchristian@gmail.com>
2016-09-02 11:52:09 -0500

    edited api call

--- app/core/weather.service.js
+++ app/core/weather.service.js
@@ -16,12 +16,11 @@
 			return locationService.getLocation().then(weather);
 
 			function weather(data) {
-				var URL = 'http://api.openweathermap.org/data/2.5/weather?q=';
 				var zipCountry = data.postal + ',' + data.country;
-				var API = '&APPID=f72650c40fef189a346723016e3b5ca6';
-				var tempUnits = '&units=imperial';
-				var callback = '&callback=JSON_CALLBACK';
-				return $http.jsonp(URL + zipCountry + API + tempUnits + callback, {cache: true})
+				return $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q='
+									+ zipCountry
+									+ '&APPID=f72650c40fef189a346723016e3b5ca6&units=imperial&callback=JSON_CALLBACK',
+									{cache: true})
 					.then(getWeatherComplete);
 					//.catch("WeatherService Error");
 					//"cod":401
