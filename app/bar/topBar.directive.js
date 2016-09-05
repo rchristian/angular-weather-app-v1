d64c6ab8ccff580887f1cd9175cb1e07700b4a08
Rachel <racheldchristian@gmail.com>
2016-09-05 14:54:10 -0500

    changed code back

--- app/bar/topBar.directive.js
+++ app/bar/topBar.directive.js
@@ -10,8 +10,30 @@
 			templateUrl: 'app/bar/topBar.html',
 			restrict: 'E',
 			scope: {},
-			controller: 'WeatherController',
+			controller: 'TopBarController',
 			controllerAs: 'vm'
 		};
+	}
+
+	function TopBarController() {
+		var vm = this;
+
+		vm.celsClass = 'unselected';
+		vm.changeTempUnit = changeTempUnit;
+		vm.fahrClass = 'selected';
+
+		function changeTempUnit() {
+			if (vm.fahrClass === "selected") {
+				vm.fahrClass = 'unselected'; //F unselected
+				vm.celsClass = 'selected'; //C selected
+				//vm.temp = Math.round((vm.temp - 32) * 5 / 9); //Celsius
+				//vm.windSpeed = (vm.windSpeed * 0.44704).toFixed(1); // M/S
+			} else if (vm.celsClass === 'selected') {
+				vm.celsClass = 'unselected'; //C unselected
+				vm.fahrClass = 'selected'; //F selected
+				//vm.temp = Math.round(vm.temp * 1.8 + 32); //Fahren
+				//vm.windSpeed = (vm.windSpeed / 0.44704).toFixed(1); //MPH
+			}
+		}
 	}
 })();
