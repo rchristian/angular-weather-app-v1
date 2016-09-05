e488bc86a307c89f1c10ea7c5f57f4d00d0281b5
Rachel <racheldchristian@gmail.com>
2016-09-05 01:24:07 -0500

    changed controller to WeatherController; removed original controller code

--- app/bar/topBar.directive.js
+++ app/bar/topBar.directive.js
@@ -10,31 +10,8 @@
 			templateUrl: 'app/bar/topBar.html',
 			restrict: 'E',
 			scope: {},
-			controller: TopBarController,
+			controller: 'WeatherController',
 			controllerAs: 'vm'
 		};
 	}
-
-	function TopBarController() {
-		var vm = this;
-
-		vm.changeTempUnit = changeTempUnit;
-		vm.farClass = 'selected';
-		vm.celsClass = 'unselected';
-		
-		function changeTempUnit() {
-			if (vm.farClass === "selected") {
-				vm.farClass = 'unselected'; //F unselected
-				vm.celsClass = 'selected'; //C selected
-				//temp = Math.round((vm.temp - 32) * 5 / 9); //Celsius
-				//windSpeed = (vm.windSpeed * 0.44704).toFixed(1); // M/S
-			} else if (vm.celsClass === 'selected') {
-				vm.celsClass = 'unselected'; //C unselected
-				vm.farClass = 'selected'; //F selected
-				//temp = Math.round(vm.temp * 1.8 + 32); //Fahren
-				//windSpeed = (vm.windSpeed / 0.44704).toFixed(1); //MPH
-			}
-		}
-	}
-
 })();
