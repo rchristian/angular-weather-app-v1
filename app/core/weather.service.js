461dd6021ce3975c4bcdb6c446e80c6e05920460
Rachel <racheldchristian@gmail.com>
2017-01-03 20:23:37 -0600

    moved weather api info to server.js

--- app/core/weather.service.js
+++ app/core/weather.service.js
@@ -5,28 +5,28 @@
 		.module('app.core')
 		.factory('weatherService', weatherService);
 
-	weatherService.$inject = ['$http', 'locationService'];
+	weatherService.$inject = ['$http'/*, 'locationService'*/];
 
-	function weatherService ($http, locationService) {
+	function weatherService ($http) {
 		return {
 			getWeather: getWeather,
 		};
 
 		function getWeather() {
-			return locationService.getLocation().then(weather);
+			return weather();
 
 			function weather(data) {
-				var zipCountry = data.postal + ',' + data.country;
-				return $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q='
-									+ zipCountry
-									+ '&APPID=f72650c40fef189a346723016e3b5ca6&units=imperial&callback=JSON_CALLBACK',
-									{cache: true})
-					.then(getWeatherComplete);
-					//.catch("WeatherService Error");
-					//"cod":401
+				return $http.get('/api/weather/connect', {cache: true})
+					.then(getWeatherComplete)
+					.catch(getWeatherCompleteFailed);
 
 				function getWeatherComplete(response) {
-					return response.data;
+					return response.data.body;
+				}
+
+				function getWeatherCompleteFailed(err) {
+					console.log(err);
+					throw err;
 				}
 			}
 		}
