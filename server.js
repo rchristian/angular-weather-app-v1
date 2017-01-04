37071f2cd89190d5590d97f6a4e6a40eb85cc632
Rachel <racheldchristian@gmail.com>
2017-01-03 20:24:33 -0600

    added server functionality

--- server.js
+++ server.js
@@ -1,47 +1,67 @@
+var bodyParser = require('body-parser');
 var express = require("express");
 var favicon = require("serve-favicon");
 var morgan = require("morgan");
 var path = require("path");
 var request = require("request");
-require("request-debug")(request);
+//require("request-debug")(request);
 
 var app = express();
 
+app.use(bodyParser.json());
+
 app.use(morgan("combined", {
-	skip: function(req, res) {
-		return res.statuscode < 400;
-	}
+    skip: function(req, res) {
+        return res.statuscode < 400;
+    }
 }));
 
 app.use(express.static(path.join(__dirname, "app")));
 app.use(favicon(__dirname + "/app/assets/imgs/favicon.ico"));
 
 app.use(function(req, res, next) {
-	console.log("/" + req.method);
-	next();
+    console.log("/" + req.method);
+    next();
 });
 
-// var options = {
-//
-// }
-//
-// app.get("api/weather/connect", function(req,res) {
-// 	request("weatherapi", function(err, body) {
-// 		if(err) {return err;}
-// 		if(!err && res.statuscode == 200) {
-// 			res.send(body);
-// 		}
-// 	});
-// });
+var getLocation = function(req, res, next) {
+    var url = "http://ipinfo.io";
+
+    request.get({url: url, json: true, headers: {"User-Agent": "request"}}, function(err, data) {
+        if (err) { return err; }
+        if (!err) {
+            res.setHeader("Content-Type", "application/json");
+            req.data
+            return next();
+        }
+    });
+};
+
+app.get("/api/weather/connect", getLocation, function(req, res) {
+    var userLocation = req.data;
+    console.log(userLocation);
+
+    var zipCountry = "37187" + "," + "US";
+
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + zipCountry + "&APPID=f72650c40fef189a346723016e3b5ca6&units=imperial";
+
+    request.get({url: url, json: true, headers: {"User-Agent": "request"}}, function(err, data) {
+        if (err) { return err; }
+        if(!err) {
+            res.setHeader("Content-Type", "application/json");
+            res.send(data);
+        }
+    });
+});
 
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("*", function(req, res) {
-	res.status(404).send("No such page");
+    res.status(404).send("No such page");
 });
 
 app.listen(3000, function() {
-	console.log("Listening on port 3000")
+    console.log("Listening on port 3000")
 });
