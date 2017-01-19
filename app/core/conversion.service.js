351fb166b69aee72c462481ff4f2f91cee595b76
Rachel <racheldchristian@gmail.com>
2017-01-18 22:28:03 -0600

    deleted notes

--- app/core/conversion.service.js
+++ app/core/conversion.service.js
@@ -28,22 +28,3 @@
 		}
 	}
 })();
-
-
-/*
-myApp.factory('myFactory', ['$rootScope', function ($rootScope) {
-    $rootScope.$emit("myEvent", myEventParams);
-}]);
-
-
-
-use $broadcast from the $rootScope
-listen using $on from the local $scope that needs to know about the event
-
-
-http://plnkr.co/edit/PgPBf5UjYOBBYxThNa7j?p=preview
-
-http://stackoverflow.com/questions/19446755/on-and-broadcast-in-angular
-
-http://stackoverflow.com/questions/25274563/angularjs-communication-between-directives
-*/
