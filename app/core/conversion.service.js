11667bf523ae0d67f17a2814bb8853f9b74b6fce
Rachel <racheldchristian@gmail.com>
2017-01-19 13:13:04 -0600

    typo

--- app/core/conversion.service.js
+++ app/core/conversion.service.js
@@ -11,14 +11,14 @@
 		service.tempUnits = {
 			farhenheit: 'farhenheit',
 			celsius: 'celsius'
-		}
+		};
 
 		service.selectedUnit = 'farhenheit';
 
-		service.convertTemperature = function(temp, wSpeed, tempUnit) {
-			if(service.selectedUnit === tempUnit) {
+		service.convertTemperature = function(temp, tempUnit) {
+			if (service.selectedUnit === tempUnit) {
 				return temp;
-			} else if (service.selectedUnit === service.tempUnits.farhenheit) {
+			} else if (service.selectedUnit === service.tempUnits.farhenheiht) {
 				return Math.round(temp * 1.8 + 32);
 			} else if (service.selectedUnit === service.tempUnits.celsius) {
 				return Math.round((temp - 32) * 5 / 9);
